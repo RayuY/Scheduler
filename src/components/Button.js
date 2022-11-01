e502067082bad92f9b39ffacebf4498f83c35627
Ray Yu <kaito-akira@hotmail.com>
2022-11-01 15:03:50 -0700

    Test buttons interactions on storybook complete

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,35 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
+// export default function Button(props) {
+//    return <Button>{props.children}</Button>;
+// }
+
+
 export default function Button(props) {
-   return <></>;
+  let buttonClass = classNames('button', {
+    'button--confirm': props.confirm,
+    'button--danger': props.danger
+  });
+
+  // if (props.confirm) {
+  //   buttonClass += " button--confirm";
+  // }
+
+  // if (props.danger) {
+  //   buttonClass += " button--danger";
+  // }
+
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className={buttonClass}
+    >
+      {props.children}
+    </button>
+  )
+  
 }
