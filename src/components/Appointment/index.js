83a7b49f6299da08295a082724f50ba1c2ef2a16
Ray Yu <kaito-akira@hotmail.com>
2022-11-02 17:06:08 -0700

    Add Header and Empty stories

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.scss'
+
+const index = () => {
+  return (
+    <article className="appointment"></article>
+  )
+}
+
+export default index
--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,9 @@ import DayListItem from 'components/DayListItem';
 import Button from 'components/Button';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment/index';
+import Header from 'components/Appointment/Header'
+import Empty from 'components/Appointment/Empty'
 
 // ------- Button test 
 
@@ -153,3 +156,22 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+// -------- Appointment test
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: 'white', value: '#fff', default: true }]
+  })
+  .add("Appointment", () => (
+    <Appointment />
+  ))
+  .add("Appointment with time", () => (
+    <Appointment time='12pm' />
+  ))
+  .add("Header", () => (
+    <Header time='12pm' />
+  ))
+  .add("Empty", () => (
+    <Empty onAdd={action("onAdd")} />
+  ))
